d13159c5a4b8c359cdcc349b3d81af031198c4fc
pbhanu1994 <pbhanu.1994@gmail.com>
2022-05-13 00:02:07 +1000

    Added the Table rows & columns

--- src/components/next_to_go_races/index.js
+++ src/components/next_to_go_races/index.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Typography, Paper, Tabs, Tab } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import moment from "moment";
 import { getData } from "../../services/getData";
 import RacesTable from "./RacesTable";
 
@@ -25,20 +26,80 @@ const NextToGoRaces = () => {
     getRacesData();
   }, []);
 
-  //   useEffect(() => {
-  //     var timer = setInterval(() => setDate(new Date()), 1000);
+  useEffect(() => {
+    var timer = setInterval(() => setDate(new Date()), 1000);
 
-  //     return () => clearInterval(timer);
-  //   }, [date]);
+    return () => clearInterval(timer);
+  }, [date]);
 
   const handleChange = (e, newValue) => {
     setValue(newValue);
-    // const races_data = Object.values(racesData.race_summaries);
-    //   setCategory(e.target.value.toLowerCase());
-    //   const ss = dd.filter((rc) => rc.race_name.toLowerCase().includes(category));
-    //   console.log(ss);
-    //   return ss;
+    newValue !== 0 &&
+      setCategory(
+        Object.values(racesData.race_summaries)[
+          newValue - 1
+        ].race_name.toLowerCase()
+      );
   };
+
+  const columns = [
+    {
+      field: "raceNumber",
+      headerName: "Race Number",
+      width: 350,
+      editable: true,
+    },
+    {
+      field: "meetingName",
+      headerName: "Meeting Name",
+      width: 350,
+      editable: true,
+    },
+    {
+      field: "raceTime",
+      headerName: "Race Starts at",
+      width: 350,
+      editable: true,
+    },
+  ];
+
+  const rowsData = [];
+
+  racesData.race_summaries &&
+    Object.values(racesData.race_summaries)
+      .slice(0, 5)
+      .filter((raceObj) =>
+        value !== 0
+          ? raceObj.race_name.toLowerCase().includes(category)
+          : raceObj.race_name.toLowerCase()
+      )
+      .sort((a, b) =>
+        moment(a.advertised_start.seconds * 1000)
+          .format("LTS")
+          .localeCompare(
+            moment(b.advertised_start.seconds * 1000).format("LTS")
+          )
+      )
+      .filter(
+        (item) =>
+          moment(item.advertised_start.seconds * 1000).diff(
+            moment(date),
+            "minutes"
+          ) > 1
+      )
+      .map((item, index) =>
+        rowsData.push({
+          id: index,
+          raceNumber: item.race_number,
+          meetingName: item.meeting_name,
+          raceTime: `${moment(item.advertised_start.seconds * 1000).format(
+            "h:mm:ss A"
+          )} ${moment(item.advertised_start.seconds * 1000).from(
+            moment(date)
+          )}`,
+        })
+      );
+  const rows = [...rowsData];
 
   return (
     <>
@@ -53,13 +114,14 @@ const NextToGoRaces = () => {
           textColor="primary"
           centered
         >
+          <Tab label="All" />
           {racesData.race_summaries &&
             Object.values(racesData.race_summaries)
               .slice(0, 5)
-              .map((data) => <Tab label={data.race_name} />)}
+              .map((data, index) => <Tab key={index} label={data.race_name} />)}
         </Tabs>
       </Paper>
-      <RacesTable />
+      <RacesTable columns={columns} rows={rows} />
     </>
   );
 };
